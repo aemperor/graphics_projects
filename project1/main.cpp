c11aaa701c7fc0f7adcaf3de9086d531869be8c9
Alexis Emperador <cs.utexas.edu>
2013-01-24 18:25:04 -0600

    rotate half working

--- project1/main.cpp
+++ project1/main.cpp
@@ -16,8 +16,6 @@ void Init() {
   // TODO: Change this call so that the drawing methods work on a canvas
   // that is 200x200 units with the origin (0, 0) at the center of the
   // canvas.
-  // glutInitWindowSize(200, 200);
-  // glutInitWindowPosition(0, 0);
   gluOrtho2D(-1.0, 1.0, -1.0, 1.0);
   glMatrixMode(GL_MODELVIEW);
 }
@@ -37,6 +35,7 @@ struct Point2 {
   GLfloat x, y;
 };
 
+// debugging
 // struct Point2 {
 //   Point2(GLint _x, GLint _y) {
 //     x[0] = _x;
@@ -53,24 +52,21 @@ const Color zero = Color(0, 0, 0);  // black
 const Color one = Color(1, 0, 0);  // red
 const Color two = Color(0, 1, 0);  // green
 const Color three = Color(0, 0, 1);  // blue
-const Color white = Color(1, 1, 1);  // white
 
-// Color recentColor = zero;
 Color color = zero;
-int direction = 0;  // right = 0; left = 1
+float direction[2] = {1.0, 1.0};
 int penState = 0;  // down = 0; up = 1
-float sine = 0.0;
-float cosine = 0.0;
 
 void Display() {
   glClear(GL_COLOR_BUFFER_BIT);
   // TODO: Change to draw according to turtle commands given
   // Draw a red triangle.
   if (penState == 0) {
-    glBegin(GL_LINE_STRIP);
     for (int i = 0; i < points.size(); ++i) {
       glColor3fv(color);
-      glVertex2i(points[i].x, points[i].y);
+      glBegin(GL_LINE_STRIP);
+      glVertex2i(points[i].x, .5 - points[i].y);
+      cout << "x, y" << points[i].x << "," << points[i].y << endl;
     }
     glEnd();
   }
@@ -87,6 +83,34 @@ void Display() {
   glFlush();
 }
 
+/*
+  Function changes the direction of the line based on the angle given
+  stores the new direction in the direction vector
+  dir = 0 //left [0, 1]
+  dir = 1 //right [1, 0]
+*/
+void rotate(int angle, int dir) {
+  float sine = sin(angle);
+  float cosine = cos(angle);
+  float temp1, temp2;
+
+  if (dir == 0) {
+    temp1 = (cosine * direction[0]) + ((-1 * sine) * direction[1]);
+    temp2 = (sine * direction[0]) + (cosine * direction[1]);
+    direction[0] = temp1;
+    direction[1] = temp2;
+  }
+  if (dir == 1) {
+    temp1 = (cosine * direction[1]) + ((-1 * sine) * direction[0]);
+    temp2 = (sine * direction[1]) + (cosine * direction[0]);
+    direction[0] = temp1;
+    direction[1] = temp2;
+  }
+  cout << "dir" << dir << endl;
+  cout << "rotate_direction[0]" << direction[0] << endl;
+  cout << "rotate_direction[1]" << direction[1] << endl;
+}
+
 void Interpret(const vector<Command>& commands) {
   // TODO: Interpret each command one-by-one.
   // TODO: Remove output statements as you implement them.
@@ -96,32 +120,25 @@ void Interpret(const vector<Command>& commands) {
     // cout << "commands[i]" << c.arg() << endl;
     switch (c.name()) {
     case FORWARD:
-      // cout << "before last " << endl;
+      if (points.size() <= 0)  // special case for first point
+        points.push_back(Point2(0, c.arg()));
       if (points.size() > 0) {
         last = Point2(points[points.size() - 1].x,
                       points[points.size() - 1].y);
-        // cout << "last " << endl;
-        if (direction == 0)
-          points.push_back(Point2(last.x + (sine * c.arg()),
-                           last.y + (cosine * c.arg())));
-        if (direction == 1)
-          points.push_back(Point2(last.x + (cosine * c.arg()),
-                           last.y + (sine * c.arg())));
-        direction = -1;
+        cout << "direction[0]" << direction[0] << endl;
+        cout << "direction[1]" << direction[1] << endl;
+        points.push_back(Point2(last.x + (c.arg() * direction[0]),
+                           last.y + (c.arg() * direction[1])));
       }
       // cout << "forward " << c.arg() << endl;
       break;
     case RIGHT:
-      direction = 0;
-      cosine = cos(c.arg());
-      sine = sin(c.arg());
+      rotate(c.arg(), 1);
       // cout << "right " << c.arg() << endl;
       break;
     case LEFT:
-      direction = 1;
-      cosine = cos(c.arg());
-      sine = sin(c.arg());
-      cout << "left " << c.arg() << endl;
+      rotate(c.arg(), 0);
+      // cout << "left " << c.arg() << endl;
       break;
     case PEN_UP:
       penState = 1;
